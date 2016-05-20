06951714acefd207d454b539646b548001ec4406
laurafll <laura.flores.lfl@gmail.com>
2016-05-20 18:27:30 -0400

    Initial Commit

---
+++ src/main/java/com/jalasoft/testing/pages/LeadHome.java
@@ -0,0 +1,15 @@
+package com.jalasoft.testing.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class LeadHome extends AbstractHome {
+
+    public NewLeadForm clickNewButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(newButton));
+        newButton.click();
+        return new LeadCampaignForm();
+    }
+
+}
